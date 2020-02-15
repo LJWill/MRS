67cc04e8076dca52d4c6807fc998481ce60c9f32
Xu Liu <xuliu2@student.unimelb.edu.au>
2020-02-15 15:31:00 +1100

    bug

--- Datafilter.py
+++ Datafilter.py
@@ -5,7 +5,7 @@ class filter:
     def filter(self, read_path):
         raw = pd.read_csv(read_path)
         print("Load finish")
-        result = raw.groupby("movieId")["rating"].count().reset_index(name="count")
+        result = raw.groupby("tmdbId")["rating"].count().reset_index(name="count")
         result = result.sort_values(["count"],ascending=False).head(20000)
         result.to_csv("result.csv")
 
