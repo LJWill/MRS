2c7a4538bd2fe67e382f543eb892758bb633bea7
Xu Liu <xuliu2@student.unimelb.edu.au>
2020-02-17 11:53:57 +1100

    bug

--- testreco.py
+++ testreco.py
@@ -6,24 +6,28 @@ from surprise import *
 
 class recommend:
     def recommend(self):
-        for i in range(1, 20):
+        for i in range(2, 20):
             print("genre %d start:" % i)
             (pred, algo) = dump.load("./KNNBasic/KNNBasic%d" % i)
-            df = pd.read_csv("./genre/genre%d.csv" % i, header=None)
+            list = algo.trainset.all_items()
+            list = [algo.trainset.to_raw_iid(inner) for inner in list]
+            dict = {}
+            dict["movieId"] = list
 
-            df.columns = ['movieId']
+            df = pd.DataFrame(dict)
             df.set_index(["movieId"], inplace=True)
+            # print(df)
             for j in range(0, 100):
                 df[str(j)] = None
             # print(df)
             print("loading done")
             starttime = datetime.now()
             total = df.shape[0]
-            count = 1
+            progress = 1
             for movieid in df['0'].index:
-                print("\r" + 'processing %d out of %d items...' % (count, total),
+                print("\r" + 'processing %d out of %d items...' % (progress, total),
                       end='')
-                count += 1
+                progress += 1
                 reco = algo.get_neighbors(algo.trainset.to_inner_iid(movieid), k=100)
                 reco = [algo.trainset.to_raw_iid(inner) for inner in reco]
                 count = 0
