ab822e2a45a9a629ccb51e8d69d5ffb471aa1416
Xu Liu <xuliu2@student.unimelb.edu.au>
2020-02-17 11:35:56 +1100

    add progress

--- testreco.py
+++ testreco.py
@@ -7,6 +7,7 @@ from surprise import *
 class recommend:
     def recommend(self):
         for i in range(1, 20):
+            print("genre %d start:" % i)
             (pred, algo) = dump.load("./KNNBasic/KNNBasic%d" % i)
             df = pd.read_csv("./genre/genre%d.csv" % i, header=None)
 
@@ -14,21 +15,24 @@ class recommend:
             df.set_index(["movieId"], inplace=True)
             for j in range(0, 100):
                 df[str(j)] = None
-            print(df)
+            # print(df)
             print("loading done")
             starttime = datetime.now()
-
+            total = df.shape[0]
+            count = 1
             for movieid in df['0'].index:
-
+                print("\r" + 'processing %d out of %d items...' % (count, total),
+                      end='')
+                count += 1
                 reco = algo.get_neighbors(algo.trainset.to_inner_iid(movieid), k=100)
                 reco = [algo.trainset.to_raw_iid(inner) for inner in reco]
                 count = 0
                 for movie in reco:
                     df[str(count)][movieid] = movie
                     count += 1
-                print(df)
+                # print(df)
             endtime = datetime.now()
-            df.to_csv("./genre/reco_genre%d.csv"%i)
+            df.to_csv("./genre/reco_genre%d.csv" % i)
             print("%d seconds" % (endtime - starttime).seconds)
 
 
